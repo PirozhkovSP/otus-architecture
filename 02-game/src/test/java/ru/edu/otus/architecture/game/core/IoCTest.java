7db414616a3b836842824e48a83e95700baa7da9
PirozhkovSP <isuperman98@gmail.com>
2024-02-10 20:51:46 +0300

    add concurrent test

--- 02-game/src/test/java/ru/edu/otus/architecture/game/core/IoCTest.java
+++ 02-game/src/test/java/ru/edu/otus/architecture/game/core/IoCTest.java
@@ -9,7 +9,14 @@ import org.junit.jupiter.api.Test;
 import ru.edu.otus.architecture.game.command.Command;
 import ru.edu.otus.architecture.game.core.command.CommandInit;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.function.Function;
 
 class IoCTest {
@@ -31,7 +38,7 @@ class IoCTest {
     @Test
     @DisplayName("Проверка создания помещенного в контейнер объекта в текущем скопе")
     void shouldResolveDependencyInCurrentScope() {
-        Function<Object[], Object> func = args -> (Integer)args[0] + (Integer) args[1] + (Integer) args[2];
+        Function<Object[], Object> func = args -> (Integer) args[0] + (Integer) args[1] + (Integer) args[2];
         Command command = IoC.resolve("IoC.Register", "someDependency", func);
         command.execute();
 
@@ -71,5 +78,38 @@ class IoCTest {
         ((Command) IoC.resolve("IoC.Scope.Current.Set", scope2)).execute();
 
         assertEquals(2, (Integer) IoC.resolve("someDependency"));
+    }
+
+    @Test
+    @DisplayName("По скопу на поток")
+    void eachThreadShouldHaveOunScope() throws InterruptedException, ExecutionException {
+        var pool = Executors.newFixedThreadPool(2);
+        Collection<Callable<Object>> callables = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            int finalI = i;
+            callables.add(() -> {
+                Object scope = IoC.resolve("IoC.Scope.Create");
+                ((Command) IoC.resolve("IoC.Scope.Current.Set", scope)).execute();
+                Function<Object[], Object> func = (args) -> (Object) 2;
+                ((Command) IoC.resolve("IoC.Register", "someDependency" + finalI, func)).execute();
+                return scope;
+            });
+        }
+        Collection<Future<Object>> scopesFutures = pool.invokeAll(callables);
+        List<Object> scopes = new ArrayList<>();
+        for (Future<Object> scopesFuture : scopesFutures) {
+            scopes.add(scopesFuture.get());
+        }
+        Object scope1 = scopes.get(0);
+        Object scope2 = scopes.get(1);
+        assertNotEquals(scope1, scope2);
+
+        ((Command) IoC.resolve("IoC.Scope.Current.Set", scope1)).execute();
+        assertThrows(RuntimeException.class, () -> IoC.resolve("someDependency1"));
+        assertDoesNotThrow(() -> IoC.resolve("someDependency0"));
+
+        ((Command) IoC.resolve("IoC.Scope.Current.Set", scope2)).execute();
+        assertThrows(RuntimeException.class, () -> IoC.resolve("someDependency0"));
+        assertDoesNotThrow(() -> IoC.resolve("someDependency1"));
     }
 }
